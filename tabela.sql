775c57639b90ea89b03ad7dfa44a2591edf0c3b9
Thamires de Souza Barbosa <thamiressouza2000@gmail.com>
2023-11-13 11:50:11 -0300

    Study for test

--- tabela.sql
+++ tabela.sql
@@ -1,3 +1,13 @@
+drop schema if exists spring;
+
+drop user if exists 'user'@'localhost';
+
+create schema spring;
+
+create user 'user'@'localhost' identified by 'pass123';
+
+grant select, insert, delete, update on spring.* to user@'localhost';
+
 use spring;
 
 create table voc_vocabulo (
@@ -8,6 +18,7 @@ create table voc_vocabulo (
  voc_data_hora_cadastro datetime not null,
  voc_data_hora_desativacao datetime
 );
+
 insert into voc_vocabulo (voc_termo, voc_significado, voc_versao, voc_data_hora_cadastro)
  values ('tupla', 'linha de uma tabela', 1, '2023-10-01 10:00:06'),
  ('tupla', 'conjunto de atributos relacionados', 2, current_timestamp());
